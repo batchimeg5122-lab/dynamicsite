9705fd92d1770ee6a0f61663b6ceb135d419ac5b
Aag1i <batchimeg5122@gmail.com>
2026-05-14 00:52:26 +0800

    Add files via upload

---
+++ dynamicsite.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 13, 2026 at 06:51 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dynamicsite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+-- Error reading structure for table dynamicsite.admins: #1932 - Table &#039;dynamicsite.admins&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.admins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`admins`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banners`
+--
+-- Error reading structure for table dynamicsite.banners: #1932 - Table &#039;dynamicsite.banners&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.banners: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`banners`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faqs`
+--
+-- Error reading structure for table dynamicsite.faqs: #1932 - Table &#039;dynamicsite.faqs&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.faqs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`faqs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+-- Error reading structure for table dynamicsite.gallery: #1932 - Table &#039;dynamicsite.gallery&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.gallery: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`gallery`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menus`
+--
+-- Error reading structure for table dynamicsite.menus: #1932 - Table &#039;dynamicsite.menus&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.menus: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`menus`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+-- Error reading structure for table dynamicsite.messages: #1932 - Table &#039;dynamicsite.messages&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`messages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+-- Error reading structure for table dynamicsite.news: #1932 - Table &#039;dynamicsite.news&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.news: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`news`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletter_subscribers`
+--
+-- Error reading structure for table dynamicsite.newsletter_subscribers: #1932 - Table &#039;dynamicsite.newsletter_subscribers&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.newsletter_subscribers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`newsletter_subscribers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+-- Error reading structure for table dynamicsite.pages: #1932 - Table &#039;dynamicsite.pages&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.pages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`pages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partners`
+--
+-- Error reading structure for table dynamicsite.partners: #1932 - Table &#039;dynamicsite.partners&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.partners: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`partners`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolio`
+--
+-- Error reading structure for table dynamicsite.portfolio: #1932 - Table &#039;dynamicsite.portfolio&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.portfolio: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`portfolio`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `recent_news`
+-- (See below for the actual view)
+--
+CREATE TABLE `recent_news` (
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+-- Error reading structure for table dynamicsite.services: #1932 - Table &#039;dynamicsite.services&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.services: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`services`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+-- Error reading structure for table dynamicsite.settings: #1932 - Table &#039;dynamicsite.settings&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.settings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`settings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+-- Error reading structure for table dynamicsite.team: #1932 - Table &#039;dynamicsite.team&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.team: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`team`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testimonials`
+--
+-- Error reading structure for table dynamicsite.testimonials: #1932 - Table &#039;dynamicsite.testimonials&#039; doesn&#039;t exist in engine
+-- Error reading data for table dynamicsite.testimonials: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dynamicsite`.`testimonials`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `recent_news`
+--
+DROP TABLE IF EXISTS `recent_news`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `recent_news`  AS SELECT `news`.`id` AS `id`, `news`.`title` AS `title`, `news`.`content` AS `content`, `news`.`image` AS `image`, `news`.`created_at` AS `created_at` FROM `news` ORDER BY `news`.`created_at` DESC LIMIT 0, 10 ;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
